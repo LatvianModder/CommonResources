24ec313a9f2bbadb9faa63f59a117fa7b4c108a9
LatvianModder <latvianmodder@gmail.com>
2016-07-03 17:37:32 +0300

    Gem Ores now drop gems

--- src/main/java/com/latmod/commonresources/BlockMetals.java
+++ src/main/java/com/latmod/commonresources/BlockMetals.java
@@ -8,16 +8,24 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.oredict.OreDictionary;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by LatvianModder on 02.07.2016.
@@ -90,6 +98,11 @@ public class BlockMetals extends Block
         {
             return new ItemStack(CommonResources.metals, q, meta);
         }
+
+        public boolean isGemOre()
+        {
+            return this == ORE_RUBY || this == ORE_SAPPHIRE || this == ORE_PERIDOT;
+        }
     }
 
     public BlockMetals()
@@ -100,12 +113,6 @@ public class BlockMetals extends Block
         setHardness(3F);
         setResistance(5F);
         setSoundType(SoundType.STONE);
-    }
-
-    @Override
-    public int damageDropped(IBlockState state)
-    {
-        return getMetaFromState(state);
     }
 
     @Override
@@ -145,14 +152,6 @@ public class BlockMetals extends Block
         return getDefaultState().withProperty(VARIANT, EnumType.byMetadata(meta));
     }
 
-    /*
-    @Override
-    public MapColor getMapColor(IBlockState state)
-    {
-        return ((BlockPlanks.EnumType) state.getValue(VARIANT)).getMapColor();
-    }
-    */
-
     @Override
     public int getMetaFromState(IBlockState state)
     {
@@ -164,5 +163,72 @@ public class BlockMetals extends Block
     protected BlockStateContainer createBlockState()
     {
         return new BlockStateContainer(this, VARIANT);
+    }
+
+    @Override
+    @Nullable
+    public Item getItemDropped(IBlockState state, Random rand, int fortune)
+    {
+        return state.getValue(VARIANT).isGemOre() ? CommonResources.materials : Item.getItemFromBlock(this);
+    }
+
+    @Override
+    public int quantityDropped(IBlockState state, int fortune, @Nonnull Random random)
+    {
+        return state.getValue(VARIANT).isGemOre() ? (1 + random.nextInt(2)) : 1;
+    }
+
+    @Override
+    public int quantityDroppedWithBonus(int fortune, @Nonnull Random random)
+    {
+        if(fortune > 0 && Item.getItemFromBlock(this) != this.getItemDropped(this.getBlockState().getValidStates().iterator().next(), random, fortune))
+        {
+            int i = random.nextInt(fortune + 2) - 1;
+
+            if(i < 0)
+            {
+                i = 0;
+            }
+
+            return this.quantityDropped(random) * (i + 1);
+        }
+        else
+        {
+            return this.quantityDropped(random);
+        }
+    }
+
+    @Override
+    public int getExpDrop(IBlockState state, IBlockAccess world, BlockPos pos, int fortune)
+    {
+        if(state.getValue(VARIANT).isGemOre())
+        {
+            return MathHelper.getRandomIntegerInRange(world instanceof World ? ((World) world).rand : new Random(), 2, 5);
+        }
+
+        return 0;
+    }
+
+    @Nonnull
+    @Override
+    public ItemStack getPickBlock(@Nonnull IBlockState state, RayTraceResult target, @Nonnull World world, @Nonnull BlockPos pos, EntityPlayer player)
+    {
+        return new ItemStack(this, 1, getMetaFromState(state));
+    }
+
+    @Override
+    public int damageDropped(IBlockState state)
+    {
+        switch(state.getValue(VARIANT))
+        {
+            case ORE_RUBY:
+                return CommonResources.materials.ruby.gem.meta;
+            case ORE_SAPPHIRE:
+                return CommonResources.materials.sapphire.gem.meta;
+            case ORE_PERIDOT:
+                return CommonResources.materials.peridot.gem.meta;
+            default:
+                return getMetaFromState(state);
+        }
     }
 }
